87c36e2e6bca7afb5e59389f506f7f4461258084
Richard Kotze <richkotze@outlook.com>
2020-11-29 15:10:18 +0000

    Long words or urls break onto newline
    This prevents horizontal scroll appearing

--- src/webview/CommitList.svelte
+++ src/webview/CommitList.svelte
@@ -20,6 +20,16 @@
 
   li.commit {
     margin-top: 2px;
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+
+    -ms-word-break: break-all;
+    word-break: break-word;
+
+    -ms-hyphens: auto;
+    -moz-hyphens: auto;
+    -webkit-hyphens: auto;
+    hyphens: auto;
   }
 
   li.commit .title .author {
