2e0ea3bc21962ffe0ca80b099d165afcfb9f0d73
Richard Kotze <richkotze@outlook.com>
2020-11-29 15:14:25 +0000

    Remove hello world cmd & rename webview panel

--- src/extension.ts
+++ src/extension.ts
@@ -6,17 +6,10 @@ import { SidePanelProvider } from "./side-panel-provider";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "git-ease.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from Git Ease!");
-    }
-  );
-  context.subscriptions.push(disposable);
 
   const provider = new SidePanelProvider(context.extensionUri);
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider("gitEasePanel", provider)
+    vscode.window.registerWebviewViewProvider("git-ease.scm-panel", provider)
   );
 }
 
