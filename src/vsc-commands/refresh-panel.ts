563f39069fdd8c281ee3a214b2aa7690c798ba9b
Richard Kotze <richkotze@outlook.com>
2021-09-25 23:05:03 +0100

    Reload all commands

--- src/vsc-commands/refresh-panel.ts
+++ src/vsc-commands/refresh-panel.ts
@@ -1,10 +1,14 @@
 import * as vscode from "vscode";
 import { gitLog } from "../messages/git-log";
+import { gitRemotesList } from "../messages/git-remotes-list";
+import { linkPatternList } from "../messages/link-pattern-list";
 import { SidePanelProvider } from "../side-panel-provider";
 
 export function refreshPanel(sidePanel: SidePanelProvider): vscode.Disposable {
   return vscode.commands.registerCommand("git-ease.refresh-panel", function () {
     if (sidePanel.view) {
+      gitRemotesList(sidePanel.view);
+      linkPatternList(sidePanel.view);
       gitLog(sidePanel.view);
     }
   });
